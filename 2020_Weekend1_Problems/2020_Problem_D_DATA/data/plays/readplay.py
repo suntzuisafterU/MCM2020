9c916c6b13f543caee65840a9dfa59df1ec4db42
Aaron Toderash (adt150) <aaron.t@usask.ca>
2020-02-15 16:39:09 -0600

    house keeping

--- 2020_Weekend1_Problems/2020_Problem_D_DATA/data/plays/readplay.py
+++ 2020_Weekend1_Problems/2020_Problem_D_DATA/data/plays/readplay.py
@@ -10,6 +10,20 @@ def readplay(path):
 
     return data.values
 
+def read_a_play_for_spectral_passing_adj_mats(path):
+    f = open(path)
+
+    data = pd.read_csv(path)
+    data = data[(data.EventSubType == "Cross") | (data.EventSubType == "Hand pass") | (
+            data.EventSubType == "Head pass") | (data.EventSubType == "High pass") | (
+                            data.EventSubType == "Launch") | (data.EventSubType == "Simple pass") | (
+                            data.EventSubType == "Smart pass") |
+                    (data.TeamID == 'Huskies') &
+                    (data.DestinationPlayerID != None)]
+
+    return data.to_dict(orient='records')
+
+
 def read_a_set_of_plays(pathglob):
     paths = glob.glob(pathglob)
 
