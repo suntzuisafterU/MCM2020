3f7dbdabc51f4ac481b54eee2346a0daee608756
Evan Semenoff <ets994@mail.usask.ca>
2020-02-16 00:37:04 -0600

    yeet

--- 2020_Weekend1_Problems/2020_Problem_D_DATA/plots.py
+++ 2020_Weekend1_Problems/2020_Problem_D_DATA/plots.py
@@ -14,13 +14,14 @@ def plot2d(playlist, funclist, plottype, smoother=None):
 
         for func in funclist:
             g, b = random(), random()
-            for i in range(21, 22):
-
+            for i in range(30,31):  # this parameter is EXTREMELY sensitive actually a window
+                                    # size of ~30 gives a strong relation to alg-con
                 xs, ys = windower(i, func, play)
                 if smoother is not None:
                     xs = smoothing(xs, smoother)
                     ys = smoothing(ys, smoother)
-                plottype(xs, ys, color=(float("0."+str(i)), g, b))
+                plottype(xs, ys, label=str(func), color=(float("0."+str(i)), g, b))
+
     plt.show()
 
 
@@ -28,27 +29,43 @@ def plot3d(playlist, funclist, smoother=None):
 
     mysurf = plt.figure()
     ax = mysurf.add_subplot(111, projection='3d')
-
+    xss, yss, zss = [], [], []
     for play in playlist:
-        for i in range(1, 99):
+        for i in range(30,40):
             for func in funclist:
                 xs, ys, zs = windower3d(i, func, play)
-                if smoother is not None:
-                    xs = smoothing(xs, smoother)
-                    ys = smoothing(ys, smoother)
-                    zs = smoothing(zs, smoother)
 
-    ax.scatter(xs, ys, zs, cmap=cm.coolwarm)
+                xss += xs
+                yss += ys
+                zss += zs
+
+    if smoother is not None:
+        xss = smoothing(xss, smoother)
+        yss = smoothing(yss, smoother)
+        zss = smoothing(zss, smoother)
+    ax.scatter(xss, yss, zss, cmap=cm.coolwarm)
     plt.show()
 
 
 
 if __name__ == "__main__":
+    mysmoother = gaussian(3, 3, 50)
 
     gameglob = input("what glob friend? ")
     plays = read_glob_of_plays("data/plays/" + gameglob)
-    mysmoother = gaussian(3, 3, 50)
-    funcs = [algebraic_connectivity, tempoEV]
 
-    plot2d(plays, funcs, plt.scatter, mysmoother)
-    #plot3d(plays, funcs)
+
+    funcs = [algebraic_connectivity]
+
+    plot2d(plays, funcs, plt.plot, mysmoother)
+
+    funcs = [flowEV]
+
+    plot2d(plays, funcs, plt.plot, mysmoother)
+
+    funcs = [algebraic_connectivity, flowEV, shotsEV]
+
+    plot2d(plays, funcs, plt.plot, mysmoother)
+    #
+    # funcs = [flowEV]
+    # plot3d(plays, funcs, mysmoother)
