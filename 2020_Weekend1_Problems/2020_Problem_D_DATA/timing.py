bf0e6e7eb04db44f12871b09f08a23ab749fe7bd
Evan Semenoff <ets994@mail.usask.ca>
2020-02-15 22:22:43 -0600

    All the cool plotting stuff

--- 2020_Weekend1_Problems/2020_Problem_D_DATA/timing.py
+++ 2020_Weekend1_Problems/2020_Problem_D_DATA/timing.py
@@ -1,29 +1,14 @@
-from readplay import readplay
 
 def normtime(play):
-
     data = play
-
-    myoffset = data[0][5]
-
+    myoffset = data[0]["EventTime"]
     for i in data:
         i[5] -= myoffset
-
     return data
 
 
 def playlen(play):
+    return abs(play[0]["EventTime"] - play[-1]["EventTime"])
 
-    return abs(play[0][5] - play[-1][5])
-
-
-def avgplaylen():
-
-    t = 0
-
-    for i in range(1, 1682):
-        t += playlen(i)
-    
-    return t / 1682   
 
 
--- 2020_Weekend1_Problems/2020_Problem_D_DATA/value.py
+++ 2020_Weekend1_Problems/2020_Problem_D_DATA/value.py
@@ -1,51 +1,13 @@
-from readplay import readplay
 from timing import *
 from scipy.stats import linregress
-from os import listdir
 import glob
 
 
-class Play:
-    
-    def __init__(self, bEV, tEV, fEV, sEV, play):
-        self.bEV = bEV
-        self.tEV = tEV
-        self.fEV = fEV
-        self.sEV = sEV
-        self.TEV = sum([bEV, tEV, fEV, sEV])
-        # TODO: Extract actual play number here
-        self.playfile = play
-
-    def query(self):
-        
-        print()
-        print("For Play Number", self.playfile)
-        print("Breadth EV was:", self.bEV)    
-        print("Tempo EV was:",   self.tEV)    
-        print("Flow EV was:",    self.fEV)    
-        print("Shots EV was:",   self.sEV)    
-        print("Total EV was",    self.TEV)
-        
-        
-
-def EV(path):
-
-    myplay = readplay(path)
-            
-    bEV = breadthEV(myplay)
-    tEV = tempoEV(myplay)
-    fEV = flowEV(myplay)
-    sEV = shotsEV(myplay)
-
-    return Play(bEV, tEV, fEV, sEV, path)
-
-
 def breadthEV(play):
-
     playerset = set()
     for i in play:
         if i[1] == "Huskies":
-            playerset = playerset | set(i[2])
+            playerset = playerset | set(i["OriginPlayerID"])
 
     return len(playerset) 
 
@@ -53,7 +15,7 @@ def breadthEV(play):
 def ballX(play):
     xpos = 0
     for i in play:
-        xpos += i[9]
+        xpos += i["EventOrigin_x"]
     xpos = xpos / len(play)
     return xpos
 
@@ -61,19 +23,19 @@ def ballX(play):
 def ballY(play):
     ypos = 0
     for i in play:
-        ypos += i[10]
+        ypos += i["EventOrigin_y"]
     ypos = ypos / len(play)
     return ypos
 
-def tempoEV(play):
 
+
+def tempoEV(play):
     passtime = 0
     totalpass = 0
     for i in range(len(play) - 1):
-        if play[i][1] == "Huskies" and play[i+1][1] == "Huskies":
-            passtime += (play[i+1][5] - play[i][5])
+        if play[i][1] == "Huskies" and play[i+1]["TeamID"] == "Huskies":
+            passtime += (play[i+1]["EventTime"] - play[i]["EventTime"])
             totalpass += 1
-
 
     if totalpass != 0:
         return passtime / totalpass
@@ -81,12 +43,12 @@ def tempoEV(play):
         return 0    
 
 
-def flowEV(play):
 
+def flowEV(play):
     flowmetric = 1
 
     for i in play:
-        if i [1] == "Huskies":
+        if i["TeamID"] == "Huskies":
             flowmetric += 1.01
         else:
             flowmetric -= 0.99
@@ -95,33 +57,11 @@ def flowEV(play):
 
 
 def shotsEV(play):
-
     shots = 0
     for i in play:
-        if i[7] == "Shot" and i[1] == "Huskies":
+        if i["EventType"] == "Shot" and i["TeamID"] == "Huskies":
             shots += 10
-        elif i[7] == "Shot":
+        elif i["EventType"] == "Shot":
             shots -= 10
-
     return shots
 
-if __name__ == "__main__":
-    schema = input("Path glob for files? ")
-
-    playlist = glob.glob(schema)
-
-    res = []
-    for play in playlist:
-        res.append(EV(play))
-
-    res.sort(key=lambda x: x.sEV)
-
-    shots = [i.sEV for i in res]
-    tempo = [i.tEV for i in res]
-    breadth = [i.bEV for i in res]
-    flow = [i.fEV for i in res]
-
-    #for i in playlist:
-    #    i.query()
-
-    print(linregress(breadth, tempo))
