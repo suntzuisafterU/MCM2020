a6d5c171d73ecef52ba00becfcc86d61b47baaaf
Evan Semenoff <ets994@mail.usask.ca>
2020-02-16 22:36:52 -0600

    fixed tempo

--- 2020_Weekend1_Problems/2020_Problem_D_DATA/plots.py
+++ 2020_Weekend1_Problems/2020_Problem_D_DATA/plots.py
@@ -17,8 +17,6 @@ def plot2d(playlist, funclist, plottype, smoother=None):
             for i in range(30,31):  # this parameter is EXTREMELY sensitive actually a window
                                     # size of ~30 gives a strong relation to alg-con
                 xs, ys = windower(i, func, play)
-                ymax = max(ys)
-                ys = [i/ymax for i in ys]
                 if smoother is not None:
                     xs = smoothing(xs, smoother)
                     ys = smoothing(ys, smoother)
@@ -53,28 +51,19 @@ def plot3d(playlist, funclist, smoother=None):
 if __name__ == "__main__":
 
 
-    mysmoother = gaussian(3, 3, 14)
+    mysmoother = gaussian(3, 3, 15)
 
-    gameglob = int(input("what game friend? "))
-
-
-
-
-    plays_first =  read_glob_of_plays("data/games/game" + f"{gameglob:02}" + "_1H")
-    plays_second = read_glob_of_plays("data/games/game" + f"{gameglob:02}" + "_2H")
-    gamedata = []
-    f = open("data/matches.csv", "r")
-    f.readline()
-    for line in f:
-        gamedata =line.rstrip().split(",")
-        if int(gamedata[0]) == gameglob:
-            break
-
-    print(gamedata)
+    gameglob = input("what glob friend? ")
 
 
 
-    funcs = [diadic_sum, triad_sum, shotsEV, algebraic_connectivity]
-    plot2d(plays_first, funcs, plt.plot, mysmoother)
-    plot2d(plays_second, funcs, plt.plot, mysmoother)
+    plays_first = read_glob_of_plays(gameglob)
+
+    # plays_first =  read_glob_of_plays("data/games/game" + f"{gameglob:02}" + "_1H")
+    # plays_second = read_glob_of_plays("data/games/game" + f"{gameglob:02}" + "_2H")
+
+
+    funcs = [algebraic_connectivity, tempoEV]
+    plot2d(plays_first, funcs, plt.plot)
+   # plot2d(plays_second, funcs, plt.plot, mysmoother)
 
